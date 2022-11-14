c9d3e1bf40fe784050bc5744e23b229b362a2603
鲁黎 <luli15826195607@gmail.com>
2022-11-14 14:23:42 +0800

    添加 MultiOption 测试内容

--- Args/Args/Arguments.cs
+++ Args/Args/Arguments.cs
@@ -5,26 +5,62 @@ namespace Args
 {
     public class Arguments
     {
-        public static T Parse<T>(String param)
+        public static T Parse<T>(params String[] args)
         {
             PropertyInfo[] props = typeof(T).GetProperties();
-            foreach (PropertyInfo prop in props)
+            List<Object> objects = new List<Object>();
+            foreach (var item in props)
             {
-                object[] attrs = prop.GetCustomAttributes(true);
-                foreach (object attr in attrs)
+                objects.Add(parseOption(args.ToList(), item));
+            }
+            return (T?)Activator.CreateInstance(typeof(T), args: objects.ToArray());
+        }
+
+        private static Object parseOption(List<String> args, PropertyInfo prop)
+        {
+            object[] attrs = prop.GetCustomAttributes(true);
+            foreach (object attr in attrs)
+            {
+                var option = attr as OptionAttribute;
+                if (option != null)
                 {
-                    var option = attr as OptionAttribute;
-                    if (option != null)
+                    if (prop.PropertyType == typeof(Boolean))
                     {
-                        if (param.Contains(option.Value))
+                        foreach (var item in args)
                         {
-                            return (T?)Activator.CreateInstance(typeof(T), args: true);
+                            if (item.Contains(option.Value))
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                    else if (prop.PropertyType == typeof(UInt16))
+                    {
+                        for (int i = 0; i < args.Count; i++)
+                        {
+                            if (args[i].Contains(option.Value) && i < (args.Count - 1))
+                            {
+                                if (UInt16.TryParse(args[i + 1], out var value))
+                                {
+                                    return value;
+                                }
+                            }
+                        }
+                    }
+                    else if (prop.PropertyType == typeof(String))
+                    {
+                        for (int i = 0; i < args.Count; i++)
+                        {
+                            if (args[i].Contains(option.Value) && i < (args.Count - 1))
+                            {
+                                return args[i + 1];
+                            }
                         }
                     }
                 }
             }
 
-            return (T?)Activator.CreateInstance(typeof(T), args: false);
+            return null;
         }
     }
 }
--- Args/ArgsTest/ArgsTest.cs
+++ Args/ArgsTest/ArgsTest.cs
@@ -28,8 +28,38 @@ namespace ArgsTest
             var option = Arguments.Parse<BooleanOption>(String.Empty);
             Assert.IsFalse(option.logging);
         }
+        public record BooleanOption([property: Option("-l")] Boolean logging);
 
-        public record BooleanOption([property:Option("-l")] Boolean logging);
 
+        [Test]
+        public void should_get_port_as_option_value()
+        {
+            var option = Arguments.Parse<UInt16Option>("-p", "8080");
+            Assert.IsTrue(option.port == 8080);
+        }
+
+        public record UInt16Option([property:Option("-p")]UInt16 port);
+
+        [Test]
+        public void should_get_directory_as_option_value()
+        {
+            var option = Arguments.Parse<StringOption>("-d", "/usr/logs");
+            Assert.IsTrue(option.directory=="/usr/logs");
+        }
+
+        public record StringOption([property: Option("-d")]String directory);
+
+        [Test]
+        public void should_parse_multi_option()
+        {
+            var option = Arguments.Parse<MultiOption>("-l", "-p", "8080", "-d", "/usr/logs");
+            Assert.IsTrue(option.logging);
+            Assert.IsTrue(option.port == 8080);
+            Assert.IsTrue(option.directory == "/usr/logs");
+        }
+
+        public record MultiOption([property: Option("-l")] Boolean logging, 
+                                    [property: Option("-p")] UInt16 port, 
+                                    [property: Option("-d")] String directory);
     }
 }
